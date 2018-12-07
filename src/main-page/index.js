1d15b0f7e2fd0dfb529a657c84e7f9bb27b9710b
Nina Fröberg <ninaspostadress@gmail.com>
2018-12-07 15:05:02 +0100

    Added json for houses.

--- src/main-page/index.js
+++ src/main-page/index.js
@@ -1,13 +1,35 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './main-page.css';
 import Header from './header';
 
 class App extends Component {
+  state = {}
+
+  componentDidMount(){
+    this.fetchHouses();
+  }
+
+  fetchHouses = () => {
+    fetch('/houses.json')
+    .then(rsp => rsp.json())
+    .then(allHouses => {
+      this.allHouses = allHouses;
+      this.determineFeaturedHouse();
+    })
+  }
+
+  determineFeaturedHouse = () => {
+    if(this.allHouses){
+      const randomIndex = Math.floor(Math.random() * this.allHouses.length);
+      const featuredHouse = this.allHouses[randomIndex];
+      this.setState({ featuredHouse});
+    };
+  }
+
   render() {
     return (
       <div className="container">
-        <Header/>
+        <Header subtitle="Providing houses all over the world"/>
       </div>
     );
   }
